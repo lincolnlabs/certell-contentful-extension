5b6e313cd7be4de921d52008738ab523b4b5dd1f
Jarrod  Luca <jarrodluca@Jarrods-Air.lan>
2021-03-09 15:40:12 -0500

    finish math ediutor

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -2,41 +2,49 @@ import React, {useState, useEffect} from 'react';
 import { Paragraph } from '@contentful/forma-36-react-components';
 import { SingleLineEditor } from '@contentful/field-editor-single-line';
 import { FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
-// import Latex from './Latex';
-// const Latex = require('react-latex');
+import { Editor } from '@tinymce/tinymce-react';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
 const Field = (props: any) => {
-  let [math, setMath] = useState('');
 
+  const handleEditorChange = (content: any, editor: any) => {
+    console.log('Content was updated:', content);
+    props.sdk.field
+      .setValue(content)
+        .then((data: any) => {
+          return;
+        })
+        .catch((err: any) => {
+          return;
+        });
+  }
 
   useEffect(() => {
-    props.sdk.window.updateHeight(600);
+    props.sdk.window.updateHeight(400);
+    // Handler for external field value changes.
+    // Callback for changes of the field value.
+    
   })
 
-  return <div>
-    {/* <div className="katex-keyboard">
-      <button dangerouslySetInnerHTML={{__html:sqrt}} onClick={handleMathClick}></button>
-      <button data-tex="\\sqrt{\\sqrt{\left(\right)}}" dangerouslySetInnerHTML={{__html:sqrt2}} onClick={handleMathClick}></button>
-      <button dangerouslySetInnerHTML={{__html:superscript}} onClick={handleMathClick}></button>
-      <button dangerouslySetInnerHTML={{__html:subscript}} onClick={handleMathClick}></button>
-      <button dangerouslySetInnerHTML={{__html:times}} onClick={handleMathClick}></button>
-      <button dangerouslySetInnerHTML={{__html:div}} onClick={handleMathClick}></button>
-      <button dangerouslySetInnerHTML={{__html:frac}} onClick={handleMathClick}></button>
-      <button dangerouslySetInnerHTML={{__html:emptyset}} onClick={handleMathClick}></button>
-    </div> */}
-    {/* <SingleLineEditor field={props.sdk.field} locales={props.sdk.locales} /> */}
-    <Paragraph>Formula editor</Paragraph>
-    {/* <div style={{margin:'40px 0'}}>
-      <p dangerouslySetInnerHTML={{__html:math}}></p>
-    </div> */}
-    <div>
-      {/* <Latex>What is $(3\times 4) \div (5-3)$</Latex> */}
-    </div>
-    </div>
+  return <Editor
+  apiKey='imnh9rw9d2di202xhvrrls396z0di9582tj4q9oedgh3v0rs' 
+  initialValue="<p>This is the initial content of the editor</p>"
+  init={{
+    height: 200,
+    menubar: false,
+    external_plugins: {
+      'tiny_mce_wiris':'https://certell-contentful-extension.s3.amazonaws.com/plugin.min.js'
+    },
+    plugins: [
+    ],
+    toolbar:
+      'tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry'
+  }}
+  onEditorChange={handleEditorChange}
+/>
 };
 
 export default Field;
